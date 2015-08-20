11be4476d526f0c031a6edc7ed1f6360e058b3e6
felipeanchieta <felipeanchieta@hotmail.com.br>
2015-08-20 20:37:40 -0300

    Initial commit of the PSO algorithm

---
+++ pso.py
@@ -0,0 +1,21 @@
+from itertools import product
+
+#inertial coefficient 
+w = 0.5
+
+#learning rates
+c1, c2 = 2.0
+
+#initial values of velocity and position
+v[0][0] = 1.0
+p[0][0] = 1.0
+g[0][0] = 1.0
+x[0][0] = 1.0
+
+
+def pso():
+	for i, j in product(range(1, n + 1), range(1, d + 1)):
+		rand1 = randint(1,100) / 100.0
+		rand2 = randint(1,100) / 100.0
+		v[i][j] = w * v[i][j] + c1 * rand1 * (p[i][j] - x[i][j]) + c2 * rand2 * (glob[j] - x[i][j]
+
