6c139cc6f8630bdd2a72009751a96eaf3afd9a4f
Rodrigo Martins de Oliveira <allrod5@users.noreply.github.com>
2015-08-20 14:17:09 -0300

    Adding generation reproduction mechanics

--- mGenetic.py
+++ mGenetic.py
@@ -1,6 +1,11 @@
 import sys
 import getopt
-import genetics
+from genetics import reproduce
+from genetics import mutate
+
+def printpop(population, fitness):
+    for i in range(0, len(population)):
+        print(population[i], fitness[i])
 
 def main(argv):
 	module_name = ''
@@ -19,7 +24,77 @@ def main(argv):
 
 	module = __import__(module_name)
 
-    pass
+    popsize = 40
+    variation = [4,14]  
+    die = 0.40 
+    kill_limit = die*popsize
+    maxi = 0
+    generations = 1
+
+    population = []
+    fitness = []
+    for i in range(0,popsize):
+        population.append([1,2,3,4,5,6,7,8])
+        a = 0
+        while a < randint(variation[0],variation[1]):
+            a += 1
+            population[i] = mutate(population[i])
+        fitness.append(check(population[i]))
+
+    # Generations loop
+    while maxi != 8:
+        
+        #print("Starting gene pool:")
+        #printpop(population, fitness)
+        
+        # Picks top genes to be parents, kills rest
+        killed = 0
+        # Starting at the lowest fitness up to kill limit
+        x = 0
+        while killed < kill_limit:
+            for i in range(0,popsize):
+                try:
+                    if fitness[i] == x:
+                        population.pop(i)
+                        fitness.pop(i)
+                        killed += 1
+                    if killed == kill_limit:
+                        break
+                except:
+                    break
+            x += 1
+        
+        #print("Survial of fittest", killed, "killed off (Removed from gene pool)")
+        #printpop(population, fitness)
+        
+        children = 0
+        cpop = len(population)-1
+        while children < killed:
+            offspring = reproduce(population[randint(0,cpop)],population[randint(0,cpop)])
+            for child in offspring:
+                population.append(child)
+                fitness.append(check(child))
+            children += 2 
+        
+        #print("Reproduced", children, "children (Added to gene pool)")
+
+        # adds one to the generations
+        generations += 1
+
+        # Looks for highest fitness in the group and checks if any achieved goal
+        maxi = 0
+        for i in range(0,popsize):
+            if fitness[i] > maxi:
+                maxi = fitness[i]
+                if maxi == 8:
+                    print(population[i])
+                    break
+
+        #print("Fittest so far", maxi)
+        # printpop(population, fitness)
+        # raw_input()
+
+    print("Took", generations, "generations")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
---
+++ modules/NQueens.py
@@ -0,0 +1,20 @@
+def check(array):
+
+	collisions = 0
+	
+	for i in range(1,len(array)+1):
+		if i not in array:
+			print("DUPLICATE NUMBER ERROR - KILLING GENE")
+			return 0
+
+	for i in range(0, len(array)):
+		col = 0
+		for j in range(0, len(array)):
+			if i != j:
+				if abs(board[i]-board[j]) == abs(array[j]-array[i]):
+					col = 1
+		if col == 1:
+			collisions += 1
+
+	# return fitness
+	return len(array)-collisions
