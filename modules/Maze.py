b3b0af36af269435a1b7cecdc7e470500dc3be9a
ufabc <ufabc@localhost.localdomain>
2015-08-21 21:16:14 -0300

    nothing new below the sun

--- modules/Maze.py
+++ modules/Maze.py
@@ -1,62 +1,55 @@
 import global_setting as this
 
+# the maze is maked by a depth-first algorithm, so it's always solvable
+# at least it's what is visible from the class examples
+
+# our maze is always squared, n x n
+
+def make_maze(n):
+
+    # http://rosettacode.org/wiki/Maze_generation#Python
+
+    vis = [[0] * n + [1] for _ in range(n)] + [[1] * (n + 1)]
+    ver = [["|  "] * n + ['|'] for _ in range(n)] + [[]]
+    hor = [["+--"] * n + ['+'] for _ in range(n + 1)]
+
+    def walk(x, y):
+        vis[y][x] = 1
+
+        d = [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]
+        shuffle(d)
+        for (xx, yy) in d:
+            if vis[yy][xx]: continue
+            if xx == x: hor[max(y, yy)][x] = "+  "
+            if yy == y: ver[y][max(x, xx)] = "   "
+            walk(xx, yy)
+
+    walk(randrange(n), randrange(n))
+    for (a, b) in zip(hor, ver):
+        print(''.join(a + ['\n'] + b))
+
+
 def populate():
     this.int_a = int(input("Board size: "))
     this.population = []
     this.fitness = []
+    this.block_size = 2
 
-    #def make_maze(w = 8, h = 4):
-    w = 8
-    h = 4
-    vis = [[0] * w + [1] for _ in range(h)] + [[1] * (w + 1)]
-    nowalls = []
-    x = randrange(h)
-    y = randrange(w)
+    # generate the maze
+    make_maze(this.int_a)
 
-    #def walk(x, y):
-    vis[x][y] = 1
-	d = [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]
-	shuffle(d)
-
-    for (xx, yy) in d:
-    	if vis[xx][yy]:
-            continue
-
-    	nowalls.append((x,y,xx,yy))
-    	walk(xx, yy)
-
-    # walk(randrange(h), randrange(w))
-    nw = nowalls #?
-
-    ver = [["|  "] * w + ['|'] for _ in range(h)] + [[]]
-    hor = [["+--"] * w + ['+'] for _ in range(h + 1)]
-
-    for (x,y,xx,yy) in nowalls:
-    	print(x,y,xx,yy)
-    	if xx == x: ver[x][max(y, yy)] = "   "
-    	if yy == y: hor[max(x, xx)][y] = "+  "
-
-    for (a, b) in zip(hor, ver):
-    	print(''.join(a + ['\n'] + b))
-
-    def posabs(x, y, w):
-    	return(x*w+y)
-
-    def posidx(pos, w):
-    	return(pos//w, pos%w)
-
-    def nowallsabs(nowallsidx, w = 8):
-        return [(posabs(x,y,w), posabs(xx,yy,w)) for (x,y,xx,yy) in nowallsidx]
-
-    # nw = make_maze()
-    # draw_maze(nw)
-    nwabs = nowallsabs(nw)
-
-
+    # do the thing
+    for i in range(this.popsize):
+		this.population[i].append([])
+		for j in range(this.dimensions*this.dimensions*this.dimensions):
+			tmp = [int(x) for x in bin(randint(0,3))[2:]]
+			while len(tmp)!=this.block_size:
+				tmp.insert(0,0)
+			this.population[i][j] += tmp
 
 
 def check(array):
 
 
 def stopCriteria():
-    #return current_pos = final_pos
+    #return current_pos == final_pos
